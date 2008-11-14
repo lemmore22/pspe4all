24fdced319a9ab97f7c5a9ce5d0b3b18a80cf565
hlide <hlide@free.fr>
2008-11-14 23:07:54 +0000

    Still committing...

---
+++ emulator/allegrex/instructions/DCACHE_CREATE_DIRTY_EXCLUSIVE.h
@@ -0,0 +1,43 @@
+template< > struct allegrex_instruction_template_s< 0xbc180000, 0xfc1f0000 > : allegrex_instruction_unknown_s
+{
+	static allegrex_instruction_template_s &self()
+	{
+		static allegrex_instruction_template_s insn;
+		return insn;
+	}
+
+	static allegrex_instruction_s *get_instance()
+	{
+		return &allegrex_instruction_template_s::self();
+	}
+
+	virtual allegrex_instruction_s *instruction(int opcode)
+	{
+		return this;
+	}
+
+	virtual char const *opcode_name()
+	{
+		return "DCACHE_CREATE_DIRTY_EXCLUSIVE";
+	}
+
+	virtual int disassemble(unsigned int address, unsigned int insn, char *opcode_name, char *operands, char *comment)
+	{
+		::strcpy(opcode_name, this->opcode_name());
+		::strcpy(operands, "");
+		::strcpy(comment, "");
+		return 0;
+	}
+
+protected:
+	allegrex_instruction_template_s() {}
+};
+
+typedef allegrex_instruction_template_s< 0xbc180000, 0xfc1f0000 >
+	allegrex_instruction_DCACHE_CREATE_DIRTY_EXCLUSIVE_s;
+
+namespace allegrex_n
+{
+	allegrex_instruction_DCACHE_CREATE_DIRTY_EXCLUSIVE_s &DCACHE_CREATE_DIRTY_EXCLUSIVE =
+		allegrex_instruction_DCACHE_CREATE_DIRTY_EXCLUSIVE_s::self();
+}
---
+++ emulator/allegrex/instructions/VREXP2.h
@@ -0,0 +1,43 @@
+template< > struct allegrex_instruction_template_s< 0xd01c0000, 0xffff0000 > : allegrex_instruction_unknown_s
+{
+	static allegrex_instruction_template_s &self()
+	{
+		static allegrex_instruction_template_s insn;
+		return insn;
+	}
+
+	static allegrex_instruction_s *get_instance()
+	{
+		return &allegrex_instruction_template_s::self();
+	}
+
+	virtual allegrex_instruction_s *instruction(int opcode)
+	{
+		return this;
+	}
+
+	virtual char const *opcode_name()
+	{
+		return "VREXP2";
+	}
+
+	virtual int disassemble(unsigned int address, unsigned int insn, char *opcode_name, char *operands, char *comment)
+	{
+		::strcpy(opcode_name, this->opcode_name());
+		::strcpy(operands, "");
+		::strcpy(comment, "");
+		return 0;
+	}
+
+protected:
+	allegrex_instruction_template_s() {}
+};
+
+typedef allegrex_instruction_template_s< 0xd01c0000, 0xffff0000 >
+	allegrex_instruction_VREXP2_s;
+
+namespace allegrex_n
+{
+	allegrex_instruction_VREXP2_s &VREXP2 =
+		allegrex_instruction_VREXP2_s::self();
+}
