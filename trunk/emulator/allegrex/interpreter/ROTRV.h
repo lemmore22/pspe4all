c0e45395fc8c95479a3859b9bc1b30552fed8434
hlide@free.fr <hlide@free.fr@0a8c99ef-1b54-0410-94c7-39605bde6fa4>
2008-11-20 21:24:16 +0000

    fix ROTR/ROTRV for x86

--- trunk/emulator/allegrex/interpreter/ROTRV.h
+++ trunk/emulator/allegrex/interpreter/ROTRV.h
@@ -1,4 +1,5 @@
 /* ROTRV */
 void allegrex_instruction_template_s< 0x00000046, 0xfc0007ff >::interpret(processor_s &processor, u32 opcode)
 {
+  processor.doROTRV(rd(opcode), rt(opcode), rs(opcode));
 }
